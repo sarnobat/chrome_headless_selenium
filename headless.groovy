8bf3a93dc1c0cb44b5fab800ea3f6a6f9c22bc18
ssarnobat <ss401533@gmail.com>
2016-12-15 14:44:53 -0800

    Minor changes (untested)

--- headless.groovy
+++ headless.groovy
@@ -11,7 +11,7 @@ import com.google.common.collect.ImmutableList;
 
 public class Headless {
 
-	private static final String CHROMEDRIVER_PATH = "/sarnobat.garagebandbroken/trash/chromedriver";
+	private static final String CHROMEDRIVER_PATH = "/home/sarnobat/github/headless_chrome/chromedriver";
 
 	// private static final String CHROMEDRIVER_PATH =
 	// "/home/sarnobat/github/yurl/chromedriver";
