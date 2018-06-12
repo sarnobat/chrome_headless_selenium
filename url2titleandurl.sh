37c32e8225e38d1cac7c2233356bcace47880263
ssarnobat <ss401533@gmail.com>
2018-06-11 21:57:54 -0700

    Minor changes (untested)

--- url2titleandurl.sh
+++ url2titleandurl.sh
@@ -1,1 +1,1 @@
-cat <(echo "$1 :: ") <(echo "$1" | xargs -n 1 -d'\n' google-chrome --headless --dump-dom --disable-gpu | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g')
+echo "$1 :: " $(echo "$1" | xargs -n 1 -d'\n' google-chrome --headless --dump-dom --disable-gpu | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g')
