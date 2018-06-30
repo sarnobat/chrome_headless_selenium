947fc6307e38ac7d1af660ba5bdcdfeb6171e654
ssarnobat <ss401533@gmail.com>
2018-06-29 20:42:49 -0700

    Minor changes (untested)

--- 2_native/get_titles.sh
+++ 2_native/get_titles.sh
@@ -12,8 +12,8 @@ cd ~/github/chrome_headless/2_native/
 sh update_indexes.sh
 
 COUNT=${1:-2}
-
+echo $PWD
 cat tmp/urls_not_titled.txt | uniq | tail -$COUNT | tee tmp/urls_attempted.txt \
-	| xargs -n 1 -d'\n' url2titleandurl.sh \
+	| xargs -n 1 -d'\n' sh ~/github/chrome_headless/2_native/url2titleandurl.sh \
 	| tee tmp/titles_new.txt \
 	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
--- 2_native/url2titleandurl.sh
+++ 2_native/url2titleandurl.sh
@@ -1,1 +1,2 @@
-echo "$1 :: " $(echo "$1" | xargs -n 1 -d'\n' google-chrome --headless --dump-dom --disable-gpu | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g')
+# If you put debugging statements in here, make sure they go to stderr otherwise your txt output will have junk in it.
+echo "$1 :: " $(echo "$1" | /usr/bin/xargs -n 1 -d'\n' timeout 10 /usr/bin/google-chrome --headless --dump-dom --disable-gpu --tmeout=10000 | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g')
