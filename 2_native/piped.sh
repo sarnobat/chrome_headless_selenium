680740440769b9eea1a5a31909fc01d6bff3fe5e
ssarnobat <ss401533@gmail.com>
2018-06-29 19:51:46 -0700

    Minor changes (untested)

--- 2_native/piped.sh
+++ 2_native/piped.sh
@@ -1,3 +1,12 @@
+#-----------------------------------------------------------------------------
+#
+# DATE
+#     2018 Jun 29
+#
+# NOTES
+#     After chrome released --dump-dom for command line, we don't need docker or selenium.
+#
+#-----------------------------------------------------------------------------
 cd ~/github/chrome_headless/2_native/
 
 sh update_indexes.sh
@@ -6,4 +15,3 @@ cat tmp/urls_not_titled.txt | uniq | tail -2 | tee tmp/urls_attempted.txt \
 	| xargs -n 1 -d'\n' url2titleandurl.sh \
 	| tee tmp/titles_new.txt \
 	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
-#	| DISPLAY=:99 groovy title.groovy ~/github/chrome_headless/chromedriver_linux64 \
