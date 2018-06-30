b9081439d76f7b28dd433db56a07ff7b4fbcf032
ssarnobat <ss401533@gmail.com>
2018-06-29 19:52:19 -0700

    Minor changes (untested)

--- 2_native/piped.sh
+++ 2_native/piped.sh
@@ -1,17 +1,1 @@
-#-----------------------------------------------------------------------------
-#
-# DATE
-#     2018 Jun 29
-#
-# NOTES
-#     After chrome released --dump-dom for command line, we don't need docker or selenium.
-#
-#-----------------------------------------------------------------------------
-cd ~/github/chrome_headless/2_native/
-
-sh update_indexes.sh
-
-cat tmp/urls_not_titled.txt | uniq | tail -2 | tee tmp/urls_attempted.txt \
-	| xargs -n 1 -d'\n' url2titleandurl.sh \
-	| tee tmp/titles_new.txt \
-	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
+get_titles.sh
