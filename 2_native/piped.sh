4c95356b6e6fb1d11ecff6c69278f65f8e3d2745
ssarnobat <ss401533@gmail.com>
2018-06-29 19:32:27 -0700

    Minor changes (untested)

--- 2_native/piped.sh
+++ 2_native/piped.sh
@@ -2,8 +2,8 @@ cd ~/github/chrome_headless/2_native/
 
 sh update_indexes.sh
 
-cat tmp/urls_not_titled.txt | uniq | tail -2 | tmp/tee urls_attempted.txt \
+cat tmp/urls_not_titled.txt | uniq | tail -2 | tee tmp/urls_attempted.txt \
 	| xargs -n 1 -d'\n' url2titleandurl.sh \
-	| tee titles_new.txt \
-	| tee -a titles_all.txt
+	| tee tmp/titles_new.txt \
+	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
 #	| DISPLAY=:99 groovy title.groovy ~/github/chrome_headless/chromedriver_linux64 \
