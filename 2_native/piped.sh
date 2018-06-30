d8cc754596b71c02a1b89e3f74ae690a3e77207b
ssarnobat <ss401533@gmail.com>
2018-06-29 19:31:47 -0700

    Minor changes (untested)

--- 2_native/piped.sh
+++ 2_native/piped.sh
@@ -2,7 +2,7 @@ cd ~/github/chrome_headless/2_native/
 
 sh update_indexes.sh
 
-cat urls_not_titled.txt | uniq | tail -2 | tee urls_attempted.txt \
+cat tmp/urls_not_titled.txt | uniq | tail -2 | tmp/tee urls_attempted.txt \
 	| xargs -n 1 -d'\n' url2titleandurl.sh \
 	| tee titles_new.txt \
 	| tee -a titles_all.txt
