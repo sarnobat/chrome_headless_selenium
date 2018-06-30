dbe2c2c97c7ee7073af4b63744467cd452082028
ssarnobat <ss401533@gmail.com>
2018-06-29 19:53:24 -0700

    Minor changes (untested)

--- 2_native/get_titles.sh
+++ 2_native/get_titles.sh
@@ -11,7 +11,9 @@ cd ~/github/chrome_headless/2_native/
 
 sh update_indexes.sh
 
-cat tmp/urls_not_titled.txt | uniq | tail -2 | tee tmp/urls_attempted.txt \
+COUNT=${1:-2}
+
+cat tmp/urls_not_titled.txt | uniq | tail -$COUNT | tee tmp/urls_attempted.txt \
 	| xargs -n 1 -d'\n' url2titleandurl.sh \
 	| tee tmp/titles_new.txt \
 	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
