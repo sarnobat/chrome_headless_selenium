ce52a4b5777f810314b409444b96ceddc77091b4
ssarnobat <ss401533@gmail.com>
2019-09-04 16:17:16 -0700

    Minor changes (untested)

--- 3_lynx/get_titles.sh
+++ 3_lynx/get_titles.sh
@@ -13,7 +13,7 @@ sh update_indexes.sh
 
 COUNT=${1:-2}
 echo $PWD
-cat tmp/urls_not_titled.txt | uniq | tail -$COUNT | tee tmp/urls_attempted.txt \
+cat /tmp/urls_not_titled.txt | uniq | tail -$COUNT | tee /tmp/urls_attempted.txt \
 	| xargs -n 1 -d'\n' sh ~/github/chrome_headless/3_lynx/url2titleandurl.sh \
-	| tee tmp/titles_new.txt \
+	| tee /tmp/titles_new.txt \
 	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
