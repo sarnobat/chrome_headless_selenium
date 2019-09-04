6e8b99622e2928122fd5424b2a8d30907d4f19d1
ssarnobat <ss401533@gmail.com>
2019-09-04 16:23:13 -0700

    Minor changes (untested)

--- 3_lynx/url2titleandurl.sh
+++ 3_lynx/url2titleandurl.sh
@@ -1,2 +1,2 @@
 # If you put debugging statements in here, make sure they go to stderr otherwise your txt output will have junk in it.
-echo "$1 :: " $(echo "$1" | /usr/bin/xargs -n 1 -d'\n' timeout 10 lynx -source --dump -useragent L_y_n_x | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g')
+echo "$1 :: " $(echo "$1" | /usr/bin/xargs -n 1 -d'\n' timeout 10 lynx -source --dump -useragent L_y_n_x | grep '<title' | perl -pe 's{.*<title>\s*(.*)\s*</title>.*}{$1}g') | perl -pe 's{^ :: \n}{}g'
