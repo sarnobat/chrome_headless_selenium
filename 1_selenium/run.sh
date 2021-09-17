e13552eeb6d2258e6e7df8b2d767e9046ab86553
sarnobat <ss401533@gmail.com>
2021-09-16 19:07:39 -0700

    Minor changes (untested)

--- 1_selenium/run.sh
+++ 1_selenium/run.sh
@@ -5,10 +5,11 @@
 # This works over SSH on nuc (but not barebones) with no -X. 99 corresponds to port 6099.
 sudo apt install -y xvfb
 Xvfb :99 -ac
-echo 'www.bbc.co.uk' 	| DISPLAY=:99 groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 | tee out.html
+echo 'www.bbc.co.uk' | DISPLAY=:99 groovy headless.groovy  ~/github/chrome_headless/1_selenium/chromedriver_linux64 | tee out.html | groovy biggest_image.groovy 2> /dev/null
+#echo 'www.bbc.co.uk' 	| DISPLAY=:99 groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 | tee out.html
 
 # This works on nuc with chromedriver_linux64 if I use ssh -X (which sets DISPLAY to localhost:10.0) with the ssh client being my Macbook pro
-echo "www.bbc.co.uk"    | DISPLAY=:99 groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 | tee out.html
+#echo "www.bbc.co.uk"    | DISPLAY=:99 groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 | tee out.html
 echo "www.teamtalk.com" | DISPLAY=:99 groovy biggest_image.groovy | tee biggest_image.txt
 
 ########
