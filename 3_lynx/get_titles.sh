a95cfccfca30d0e066fd76eff01ef42708692552
Sridhar Sarnobat <ss401533@gmail.com>
2022-05-12 00:01:13 -0700

    Minor changes (untested)

--- 3_lynx/get_titles.sh
+++ 3_lynx/get_titles.sh
@@ -19,4 +19,4 @@ echo $PWD
 cat /tmp/urls_not_titled.txt | uniq | tail -$COUNT | tee /tmp/urls_attempted.txt \
 	| xargs -n 1 -d'\n' sh ~/github/chrome_headless/3_lynx/url2titleandurl.sh \
 	| tee /tmp/titles_new.txt \
-	| tee -a ~/sarnobat.git/db/yurl_flatfile_db/titles_all.txt
+	| tee -a ~/db.git/yurl_flatfile_db/titles_all.txt
--- update_indexes.sh
+++ update_indexes.sh
@@ -1,5 +1,5 @@
 cd ~/github/chrome_headless/2_native/
-cat  ~/db.git/yurl_queue_httpcat.txt  | grep http  | perl -pe 's{[0-9]+::[0-9]+::http}{http}g' | sort | grep -i -v '\.jpg' | grep -i -v '\.gif' | grep -i -v '\.png' | tee ~/github/chrome_headless/tmp/urls_httpcat.txt >/dev/null
+cat  ~/db.git/yurl_queue_httpcat.txt ~/db.git/yurl_flatfile_db/yurl_queue_2022.txt  | grep http  | perl -pe 's{[0-9]+::[0-9]+::http}{http}g' | sort | grep -i -v '\.jpg' | grep -i -v '\.gif' | grep -i -v '\.png' | tee ~/github/chrome_headless/tmp/urls_httpcat.txt >/dev/null
 cat ~/db.git/yurl_flatfile_db/titles_all.txt  | perl -pe 's{ :: .*}{}g' | tee ~/github/chrome_headless/tmp/titles_obtained.txt   >/dev/null
 
 
