fb97eec95f3e33d6f6d5c5a85ee7b7d04ca1ab0c
Sridhar Sarnobat <ss401533@gmail.com>
2022-07-24 18:14:45 -0700

    Minor changes (untested)

---
+++ 1_selenium/java/2021/piped.sh
@@ -0,0 +1,15 @@
+# This doesn't work. 
+#cat ~/sarnobat.git/yurl_queue_httpcat.txt  | perl -pe 's{.*http}{http}g' | grep -v null | grep -v jpg | DISPLAY=:99 xargs -n 1 -I% groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 "%"  | grep 'title>' | tee titles.txt
+#head -10 ~/sarnobat.git/yurl_queue_httpcat.txt  | perl -pe 's{.*http}{http}g' | grep -v null | grep -v jpg | grep -v "'" | DISPLAY=:99 xargs -n 1 -I% groovy headless.groovy ~/github/chrome_headless/chromedriver_linux64 "%"  | grep 'title>'  | perl -pe 's{.*(<title)}{$1}g'  | perl -pe 's{(</title>).*}{$1}g' | tee titles.txt
+#cat titles_all.txt  | perl -pe 's{[\s].*\n}{\n}g' | tee titles_attempted.txt
+#touch titles_attempted2.txt
+
+cd ~/github/chrome_headless/
+
+sh update_indexes.sh
+
+cat urls_not_titled.txt | uniq | tail -2 | tee urls_attempted.txt \
+	| xargs -n 1 -d'\n' url2titleandurl.sh \
+	| tee titles_new.txt \
+	| tee -a titles_all.txt
+#	| DISPLAY=:99 groovy title.groovy ~/github/chrome_headless/chromedriver_linux64 \
