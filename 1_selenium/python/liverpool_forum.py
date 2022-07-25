cfbb544b885a1e90a99b0cff84374d52c4124bda
Sridhar Sarnobat <ss401533@gmail.com>
2022-07-24 19:40:10 -0700

    Minor changes (untested)

---
+++ 1_selenium/python/liverpool_forum.py
@@ -0,0 +1,45 @@
+# pip3 install selenium
+# pip3 install webdriver-manager
+
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+import pyautogui
+
+url = None
+if sys.argv[1:]:
+	url=sys.argv[1]
+if url == None:
+	url="https://forums.liverpoolfc.com/search.php?do=finduser&userid=134992&starteronly=1&contenttype=vBForum_Thread"
+
+chrome_options = webdriver.chrome.options.Options()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+driver.get(url)
+time.sleep(4)
+
+
+
+
+elems = driver.find_elements("xpath", '//a[@href]')
+for elem in elems:
+    print(elem.get_attribute("href"))
+
+
+# pyautogui.keyDown('command')
+# pyautogui.press('s')
+# pyautogui.keyUp('command')
+# time.sleep(1)
+# 
+# pyautogui.typewrite('/')
+# # print(driver.title)
+# time.sleep(1)
+# pyautogui.typewrite('/tmp/')
+# time.sleep(1)
+# pyautogui.hotkey('enter')
+# time.sleep(1)
+# pyautogui.hotkey('enter')
+time.sleep(60)
