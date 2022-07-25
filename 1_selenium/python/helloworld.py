b2ec70b7dc814cbd464ffe1633c684ba5351bfb7
Sridhar Sarnobat <ss401533@gmail.com>
2022-07-24 18:19:19 -0700

    Minor changes (untested)

--- 1_selenium/python/helloworld.py
+++ 1_selenium/python/helloworld.py
@@ -1,14 +1,6 @@
 # pip3 install selenium
 # pip3 install webdriver-manager
-
 # /Volumes/numerous/usr/local/homebrew/lib/python3.9/site-packages
-
-# from selenium import webdriver
-# 
-# driver = webdriver.Chrome(executable_path="1_selenium/chromedriver.mac64.intel.2022-07")
-# driver.get("http://www.google.com")
-# print(driver.title)
-# driver.close()
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
