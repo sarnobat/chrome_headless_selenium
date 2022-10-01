3425f195203e4752af0f6d6789587153bd5863a4
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-30 19:26:34 -0700

    Minor changes (untested)

--- 1_selenium/python/facebook_images.py
+++ 1_selenium/python/facebook_images.py
@@ -31,22 +31,17 @@ def printLinksInCurrentPage(url):
 	time.sleep(5)
 
 
-url = None
-if sys.argv[1:]:
-	#url=sys.argv[1]
-	password=sys.argv[1]
-if url == None:
-	url="https://www.facebook.com/"
+
 
 chrome_options = webdriver.chrome.options.Options()
+chrome_options.add_argument("--disable-notifications");
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
-# printLinksInCurrentPage(url)
-
+url="https://www.facebook.com/"
 driver.get(url)
 time.sleep(3)
+# printLinksInCurrentPage(url)
 
-# elems = 
 # bet_fa = driver.find_element_by_id("email")
 # bet_fa.send_keys("0.00000005")
 # driver.find_elements("xpath", '//input[@id="email"]')[0]
@@ -54,7 +49,7 @@ time.sleep(3)
 ActionChains(driver).send_keys_to_element(driver.find_elements("xpath", '//input[@id="email"]')[0], "ss533@cornell.edu").perform()
 ActionChains(driver).send_keys_to_element(driver.find_elements("xpath", '//input[@id="pass"]')[0], "aize2FEN!").perform()
 driver.find_elements("xpath", '//button[@type="submit"]')[0].click()
-time.sleep(30)
+time.sleep(10)
 driver.get("https://www.facebook.com/megha.panchamukhi.7/photos_by")
 time.sleep(3)
 
