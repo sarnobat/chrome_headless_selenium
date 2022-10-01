eb37d3d53c95e9bfc64dcf4be29aa8ba3f2b1a74
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-30 19:27:08 -0700

    Minor changes (untested)

--- 1_selenium/python/facebook_images.py
+++ 1_selenium/python/facebook_images.py
@@ -12,24 +12,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pyautogui
 
-def printLinksInCurrentPage(url):
-	driver.get(url)
-	time.sleep(4)
-
-# 	printLinksInCurrentPage(url)
-	elems = driver.find_elements("xpath", '//a[@href]')
-	for elem in elems:
-		print(elem.get_attribute("href"))
-
-	print("----------------------------------------- ")
-	
-# 	elems = driver.find_elements("xpath", '//*/a[@rel="next"]')
-# 	for elem in elems:
-# 		print(elem.get_attribute("href"))
-# 		printLinksInCurrentPage(elem.get_attribute("href"))
-# 	print(elems[0].get_attribute("href"))
-	time.sleep(5)
-
 
 
 
@@ -40,11 +22,6 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 url="https://www.facebook.com/"
 driver.get(url)
 time.sleep(3)
-# printLinksInCurrentPage(url)
-
-# bet_fa = driver.find_element_by_id("email")
-# bet_fa.send_keys("0.00000005")
-# driver.find_elements("xpath", '//input[@id="email"]')[0]
 
 ActionChains(driver).send_keys_to_element(driver.find_elements("xpath", '//input[@id="email"]')[0], "ss533@cornell.edu").perform()
 ActionChains(driver).send_keys_to_element(driver.find_elements("xpath", '//input[@id="pass"]')[0], "aize2FEN!").perform()
@@ -52,13 +29,3 @@ driver.find_elements("xpath", '//button[@type="submit"]')[0].click()
 time.sleep(10)
 driver.get("https://www.facebook.com/megha.panchamukhi.7/photos_by")
 time.sleep(3)
-
-# print(driver.find_elements("xpath", '//input[@id="email"]')[0])
-# .sendKeys("your value")
-# for elem in elems:
-# 	print(elem.get_attribute("href"))
-
-# print("TODO: get subsequent pages")
-# time.sleep(15)
-
-# time.sleep(15)
