54bede1d1be3d8d3ac6004dfaf9944655328f764
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-24 17:18:36 -0700

    Minor changes (untested)

---
+++ jobs_netflix.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION		
+# DATE				[:VIM_EVAL:]strftime('%Y-%m-%d')[:END_EVAL:]
+# AUTHOR			ss401533@gmail.com                                           
+#----------------------------------------------------------------------------
+
+
+cat <<EOF | batcat --plain --paging=never --language sh --theme TwoDark
+get job urls
+------------
+python3 /Volumes/git/github/chrome_headless/urls.py "${@:-https://jobs.netflix.com/search?q=java}" | tee /tmp/urls.txt
+
+get job titles (from urls)
+-------------------------
+cat /tmp/urls.txt | grep /jobs/ |  xargs --delimiter '\n' -I% python3 -c "import bs4, requests; print('{message: <64}'.format(message=(bs4.BeautifulSoup(requests.get('%').content).title.text)) + '%')" | tee /tmp/out2.txt
+EOF
---
+++ urls.py
@@ -0,0 +1,34 @@
+# pip3 install selenium
+# pip3 install webdriver-manager
+# /Volumes/numerous/usr/local/homebrew/lib/python3.9/site-packages
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import sys
+import time
+
+# https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_mac64.zip
+# /Volumes/trash/trash/chromedriver.mac64.intel.114.0.5735.90
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(executable_path=r"/Volumes/trash/trash/chromedriver.mac64.intel.114.0.5735.90")
+print(sys.argv[1] or "https://www.google.com/")
+# driver.get("https://www.google.com")
+driver.get(str(sys.argv[1]) or "https://www.google.com/")
+print(driver.title)
+
+time.sleep(10)
+
+href_urls = []
+for link in driver.find_elements_by_tag_name("a"):
+	href = link.get_attribute("href")
+	if href != None:
+		href_urls.append(href)
+
+xs = [x for x in href_urls if x is not None]
+# for element in href_urls if element is not None:
+for element in sorted(xs):
+	print(element)
+        
+driver.close()
+
