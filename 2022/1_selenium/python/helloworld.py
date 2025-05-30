faa504a5252814b2c459e282cbe04eab14e997f0
Sridhar Sarnobat <ss401533@gmail.com>
2025-05-30 14:19:03 -0400

    2025-05-30: helloworld.py

--- 2022/1_selenium/python/helloworld.py
+++ 2022/1_selenium/python/helloworld.py
@@ -7,6 +7,6 @@ options = Options()
 options.binary_location = "/Volumes/apps/Apps/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="137.0.7151.55").install()), options=options)
-driver.get("https://www.google.com/search?num=10&sca_esv=ccd6930778b679eb&udm=8&sxsrf=AE3TifPGi3MqcAhip8htPYh_0lViQAmh-g%5C%3A1748302928481&q=java%5C%20jobs%5C%20new%5C%20jersey%5C%20Full%5C%20time%5C%20Full%5C%20time&uds=AOm0WdE2fekQnsyfYEw8JPYozOKzaXBUIHF2MdXIt8a5916iJgAvAbYuAybvl9gk5ndWSXisolfyBXqD0LnMKSpv4Br2SXLRJqPWHTSyWZUrTR2xZ_Pf9JvdO1gnWq_ardkPdFI7_lGFZjLtByXA1bT_a02Fx05JGLo9wHdEjdzS4EzzjuQdxk169oiqx25_JhkXS2EzHBY4QRbMVnYBCL1xXazyQglHGKrnm4TsarfKxjFuCkS2VG0IzwYx0pmj88gVC1W3iWcihvrx4rMhhVCAhxx7UT9WVLw1dtJeYCEvNgEpf3H90f2heNl5X9bEiagzNGpM4eg1F9KilrLgrw-IMhfxh4h88Q&ved=2ahUKEwj45ov4p8KNAxWqN2IAHQE8APkQmv0KegQIEhAG&jbr=sep:0")
+driver.get("https://www.indeed.com")
 print(driver.title)
 driver.quit()
